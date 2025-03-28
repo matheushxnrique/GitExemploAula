a95b562946138762211e7bbdedcb90de405bf112
Matheus <matheushenriquesci555@gmail.com>
2025-03-27 22:19:41 -0300

    Criação do projeto

---
+++ src/gitexemploaula/GitExemploAula.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gitexemploaula;
+
+/**
+ *
+ * @author mathe
+ */
+public class GitExemploAula {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
