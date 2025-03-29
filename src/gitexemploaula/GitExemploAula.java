5e7d5e3c8850601dd1a94d0ff191c6f38405f2fc
Matheus <matheushenriquesci555@gmail.com>
2025-03-28 23:19:05 -0300

    Segunda versão do projeto

--- src/gitexemploaula/GitExemploAula.java
+++ src/gitexemploaula/GitExemploAula.java
@@ -1,20 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package gitexemploaula;
 
-/**
- *
- * @author mathe
- */
+
 public class GitExemploAula {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Segunda versão do projeto");
+        System.out.println("Testando envio de versões");
+     
+               
     }
     
 }
